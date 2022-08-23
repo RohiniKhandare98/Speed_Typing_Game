cc33644e69f104e27b8d78ee175ebd8682ff3022
RohiniKhandare <91168473+RohiniKhandare98@users.noreply.github.com>
2022-08-23 10:43:22 +0530

    Add files via upload

---
+++ SpeedTyping/script.js
@@ -0,0 +1,63 @@
+const Random_Quote_API_URL = 'https://api.quotable.io/random';
+const quoteDisplayElement = document.getElementById('text-display');
+const quoteInputElement = document.getElementById('text-input');
+const timerElement = document.getElementById('timer');
+
+quoteInputElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span');
+    const arrayValue = quoteInputElement.value.split('');
+    let correct = true;
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index];
+        if (character == null) {
+            characterSpan.classList.remove('incorrect');
+            characterSpan.classList.remove('correct');
+            correct = false;
+        } else if (character == characterSpan.innerText) {
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect');
+        } else {
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.add('incorrect');
+            correct = false;
+        }
+    });
+
+    if (correct) {
+        renderNewQuote();
+    }
+});
+
+function getRandomQuote() {
+    return fetch(Random_Quote_API_URL)
+        .then(response => response.json())
+        .then(data => data.content);
+}
+
+async function renderNewQuote() {
+    const quote = await getRandomQuote();
+    quoteDisplayElement.innerHTML = '';
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = character;
+        quoteDisplayElement.appendChild(characterSpan);
+    });
+    quoteInputElement.value = null;
+    startTimer();
+}
+
+let startTime;
+function startTimer() {
+    timerElement.innerText = 0;
+    startTime = new Date();
+    setInterval(() => {
+        timer.innerText = getTimerTime();
+    }, 1000);
+}
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000);
+}
+
+
+renderNewQuote();
